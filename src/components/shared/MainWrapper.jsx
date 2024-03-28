57d94a59dfbb8c31aff68372c6ca369085cadfea
nazrudeen sahabudeen <118974230+nazrudeen-web@users.noreply.github.com>
2024-03-28 07:01:43 +0000

    Initial Setup

---
+++ src/components/shared/MainWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainWrapper = ({children}) => {
+  return (
+    <div className='max-w-2xl mx-auto'>
+        {children}
+    </div>
+  )
+}
+
+export default MainWrapper
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainWrapper from '../components/shared/MainWrapper'
+
+const Index = () => {
+  return (
+    <MainWrapper>
+      <h1>hell</h1>
+    </MainWrapper>
+  )
+}
+
+export default Index
