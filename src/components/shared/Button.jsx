17e736cad9debb8d72c8ed8382eee1536d175fbd
nazrudeen sahabudeen <118974230+nazrudeen-web@users.noreply.github.com>
2024-03-28 11:15:26 +0000

    root page small changes

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Button = ({ to, type, text }) => {
-  let btnStyle = `btn bg-indigo-700 hover:btn-ouline hover:text-black hover:bg-white uppercase hover:border-black w-full text-white text-base mt-4`;
+  let btnStyle = `btn bg-indigo-700  hover:text-white hover:bg-black uppercase hover:border-none w-full text-white text-base mt-4`;
   if (type === "outline") {
-    btnStyle = `btn btn-outline hover:bg-indigo-700 hover:text-white uppercase hover:border-none border-black w-full text-black text-base mt-4`;
+    btnStyle = `btn  bg-black hover:bg-indigo-700 hover:text-white uppercase hover:border-none border-black w-full text-white text-base mt-4`;
   }
   return (
     <Link to={`/${to}`} className={btnStyle}>
