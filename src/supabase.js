24f90bb0d115eaa676dc00547bb76cfd1bf2e1a5
nazrudeen sahabudeen <118974230+nazrudeen-web@users.noreply.github.com>
2024-04-01 04:01:18 +0000

    new

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+// Set environment variables
+process.env.VITE_SUPABASE_URL = "https://ksmkklbymdfzjovecvyo.supabase.co";
+process.env.VITE_SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbWtrbGJ5bWRmempvdmVjdnlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3Mzc4NDcsImV4cCI6MjAyNzMxMzg0N30.IKZEN_7NBEQGLnpfrk46xrs59eWT-v0B3WbOZ29BjeA";
+
 // Access environment variables
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
