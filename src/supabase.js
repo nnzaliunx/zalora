f92d418944e230935f3625e8aa21d99fd4506aed
nnzaliunx <nnzaliunx@gmail.com>
2024-05-09 19:37:59 +0400

    env change

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Set environment variables
-process.env.VITE_SUPABASE_URL = "https://ksmkklbymdfzjovecvyo.supabase.co";
+process.env.VITE_SUPABASE_URL = "https://tklhndtylrekoaudbjoo.supabase.co";
 process.env.VITE_SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbWtrbGJ5bWRmempvdmVjdnlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3Mzc4NDcsImV4cCI6MjAyNzMxMzg0N30.IKZEN_7NBEQGLnpfrk46xrs59eWT-v0B3WbOZ29BjeA";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbGhuZHR5bHJla29hdWRiam9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyMjY3MDIsImV4cCI6MjAzMDgwMjcwMn0.NNpgrO1NA7j3gKmpdgq8WKqDIcpqv7IYtS40wWp3HvY";
 
 // Access environment variables
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
